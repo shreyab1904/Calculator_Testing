24b067f884f0fb44a6c0dd7524339440e18567bd
shreyab1904 <shreyaborkar0201@gmail.com>
2022-03-25 15:21:10 +0530

    Chanes done

--- Test_Calculator.py
+++ Test_Calculator.py
@@ -1,27 +1,32 @@
-import main
+import Calculator
 import unittest
 
 class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.x=20
+        self.y=10
+
+    def tearDown(self) :
+        self.x=0
+        self.y=0
+
     def test_add(self):
-        x=10
-        y=20
-        result=Calculator.add(x,y)
-        self.assertEqual(result,x+y)
+        #ACT
+        result=Calculator.add(self.x,self.y)
+        #ASSERT
+        self.assertEqual(result,self.x+self.y)
+
     def test_sub(self):
-        x=20
-        y=10
-        result=Calculator.sub(x,y)
-        self.assertEqual(result,x-y)
+        result=Calculator.sub(self.x,self.y)
+        self.assertEqual(result,self.x-self.y)
+
     def test_mult(self):
-        x=10
-        y=20
-        result=Calculator.mult(x,y)
-        self.assertEqual(result,x*y)
+        result=Calculator.mult(self.x,self.y)
+        self.assertEqual(result,self.x*self.y)
+
     def test_div(self):
-        x=10
-        y=5
-        result=Calculator.div(x,y)
-        self.assertEqual(result,x/y)
+        result=Calculator.div(self.x,self.y)
+        self.assertEqual(result,self.x/self.y)
 
 if __name__=="__main__":
     unittest.main()
